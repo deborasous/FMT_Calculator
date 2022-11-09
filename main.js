2ef99edee01f3b8a16c15d37d8f5f7a3d4afd5e8
Debora Sousa <deborapatriciadp27@gmail.com>
2022-11-09 19:01:56 -0300

    Mudança de if para switch

--- main.js
+++ main.js
@@ -1,41 +1,56 @@
 let number1 = document.getElementById("number1");
 let number2 = document.getElementById("number2");
-let operation = document.getElementById("operation");
 let result = document.getElementById("result");
 let danger = document.getElementById("resDanger");
 
 document.querySelector(".btn").addEventListener("click", () => {
-  danger.style.color = "#e4e6eb";
-
+  let operation = document.getElementById("operation");
   let calc;
-  if (number1.value !== "" && number2.value !== "") {
-    if (operation.value === "sun") {
-      calc = Number(number1.value) + Number(number2.value);
-      danger.innerHTML = `${number1.value} mais ${number2.value} é:`;
-      result.innerHTML = `${calc}`;
-    } else if (operation.value === "subtraction") {
-      calc = Number(number1.value) - Number(number2.value);
-      danger.innerHTML = `${number1.value} menos ${number2.value} é:`;
-      result.innerHTML = `${calc}`;
-    } else if (operation.value === "multiplication") {
-      calc = Number(number1.value) * Number(number2.value);
-      danger.innerHTML = `${number1.value} vezes ${number2.value} é:`;
-      result.innerHTML = `${calc}`;
-    } else if (operation.value === "division") {
-      calc = Number(number1.value) / Number(number2.value);
-      danger.innerHTML = `${number1.value} dividido por ${number2.value} é:`;
-      result.innerHTML = `${calc}`;
-    } else if (operation.value === "percentage") {
-      calc = (Number(number1.value) * Number(number2.value)) / 100;
-      danger.innerHTML = `${number1.value}% por cento de ${number2.value} é:`;
-      result.innerHTML = `${calc}`;
-    }
-  } else if (number1.value == "" || number2.value == "") {
+
+  //valida se os inputs estão vazios e retorna a solicitação de números
+  if (number1.value == "" || number2.value == "") {
     danger.innerText = "Digite dois valores para calcular";
     danger.style.color = "red";
     number1.value = "";
     number2.value = "";
     result.innerHTML = 0;
+  }
+
+  //Se os inputs forem diferentes de vazio, roda o cálculo
+  if (number1.value !== "" && number2.value !== "") {
+    danger.style.color = "#e4e6eb";
+
+    switch (operation.value) {
+      case "sun":
+        calc = Number(number1.value) + Number(number2.value);
+        danger.innerHTML = `${number1.value} mais ${number2.value} é:`;
+        result.innerHTML = `${calc}`;
+        break;
+
+      case "subtraction":
+        calc = Number(number1.value) - Number(number2.value);
+        danger.innerHTML = `${number1.value} menos ${number2.value} é:`;
+        result.innerHTML = `${calc}`;
+        break;
+
+      case "multiplication":
+        calc = Number(number1.value) * Number(number2.value);
+        danger.innerHTML = `${number1.value} vezes ${number2.value} é:`;
+        result.innerHTML = `${calc}`;
+        break;
+
+      case "division":
+        calc = Number(number1.value) / Number(number2.value);
+        danger.innerHTML = `${number1.value} dividido por ${number2.value} é:`;
+        result.innerHTML = `${calc}`;
+        break;
+
+      default:
+        calc = (Number(number1.value) * Number(number2.value)) / 100;
+        danger.innerHTML = `${number1.value}% por cento de ${number2.value} é:`;
+        result.innerHTML = `${calc}`;
+        break;
+    }
   }
 });
 
@@ -44,4 +59,5 @@ document.querySelector(".btnClear").addEventListener("click", () => {
   number2.value = "";
   result.innerHTML = 0;
   danger.innerHTML = "O resultado será exibido aqui.";
+  danger.style.color = "#e4e6eb";
 });
